ce470c2a6b4c3b53559e2f581f9016d04c60bebf
Mickaël Schoentgen <contact@tiger-222.fr>
2020-01-10 10:22:35 +0100

    Integrate a simple auto-updater (fixes #10)
    I used a modified version of the one from Nuxeo Drive.
    
    It is effective on Windows only for now.

--- watermark/__init__.py
+++ watermark/__init__.py
@@ -6,9 +6,16 @@ This module is maintained by Mickaël Schoentgen <contact@tiger-222.fr>.
 You can always get the latest version of this module at:
     https://github.com/BoboTiG/watermark-me
 If that URL should fail, try contacting the author.
+
+Versionning
+-----------
+We use semantic versioning (https://semver.org) compliant with distutils
+and the PEP-440.
+To declare a beta, use this schema:
+    - X.Y.ZbN i.e. "2.4.5b1"
 """
 
-__version__ = "0.0.1"
+__version__ = "0.1b1"
 __author__ = "Mickaël 'Tiger-222' Schoentgen"
 __copyright__ = """
 Copyright (c) 2019-2020, Mickaël 'Tiger-222' Schoentgen
